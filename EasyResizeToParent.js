977cd70fe18a3864baac713456edd0a840c4c119
Maxwell Dreyster <admin@mdtech.us>
2017-01-05 18:23:25 -0500

    Complete version 1.

---
+++ EasyResizeToParent.js
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2017 Maxwell Dreytser
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+// If you would like IE 8 support, include the polyfill here: https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Compatibility
+(function(){
+	function getAllElementsWithAttribute(attribute)	{
+		// Prefer to use the native querySelectorAll if available.
+		if (document.querySelectorAll) {
+			return document.querySelectorAll('[' + attribute + ']');
+		} else {
+			// Otherwise, use custom code.
+			
+			var matches = [];
+			var allElms = document.getElementsByTagName('*');
+			
+			// Simply check every element on the page for the requested attribute.
+			for (var i = 0, n = allElms.length; i < n; i++)
+			{
+				if (allElms[i].getAttribute(attribute) !== null)
+				{
+					matches.push(allElms[i]);
+				}
+			}
+			
+			return matches;
+		}
+	}
+
+	function doResize() {
+		var elms = getAllElementsWithAttribute('resizeToParent');
+		
+		// For each element we need to resize...
+		for (var i = 0, len = elms.length; i < len; i++) {
+			var elm = elms[i];
+			var parentElm = elm.parentNode;
+
+			// Calculate the aspect ratio for the element.
+			var elmAspectRatio = elm.clientWidth / elm.clientHeight;
+			// Calculate the aspect ratio for the element's parent.
+			var parentElmAspectRatio = parentElm.clientWidth / parentElm.clientHeight;
+			
+			// If the parent's aspect ratio is larger than this element's, 
+			if (parentElmAspectRatio > elmAspectRatio) {
+				// Lock the element's height to the parent height.
+				elm.style.width = 'auto';
+				elm.style.height = parentElm.clientHeight + 'px';
+			} else {
+				// Otherwise, lock the width.
+				elm.style.width = parentElm.clientWidth + 'px';
+				elm.style.height = 'auto';
+			}
+		}
+	}
+
+	// Resize the elements every time the window is resized.
+	window.addEventListener("resize", doResize);
+	// Additionally, resize every 0.5 seconds in case a new element may be added.
+	setInterval(doResize, 500);
+	doResize();
+})();
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # EasyResizeToParent
-Simple library that resizes selected elements to their parent Div.
+A simple library that resizes selected elements to their parent Div.
+
+# Usage
+To use this library, simply add the `resizeToParent` attribute to any element.
+
+```html
+<img src="..." resizeToParent />
+```
+
+**Demo:** http://codepen.io/Maxwell175/pen/wgBNxJc
+
+# Dependencies
+**None.**
+
+## Things to note
+
+* **For IE 8 support, please include the `addEventListener` polyfill.**
+   https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#Compatibility
+   http://caniuse.com/#feat=addeventlistener
